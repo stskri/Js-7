ccac1e81073369d1ba724022c21d631a52aea12a
stskri <177446189+stskri@users.noreply.github.com>
2024-09-17 11:43:09 +0000

    [finish]js-5

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
-$(document).ready(function () {
-  $('body').html('<h1>Hello jQuery!!</h1>');
+$(function (){
+  $('.box1').slideDown(1000, function (){
+    1000
+    $('.box1').css({
+      'background-color': '#0000FF',
+      'width': '200px',
+      'height': '100px'
+    }).slideUp(1000);
+  });
 });
